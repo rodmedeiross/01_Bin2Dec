4a4200ab33abb87b3fe59658890d64a0a2b83a2c
rodmedeiross <roddmedeiross@gmail.com>
2020-07-03 14:05:08 -0300

    minor fixes

--- src/app2.js
+++ src/app2.js
@@ -4,10 +4,10 @@ function bin2dec(){
 
   if (binary.length > 8 | binary.length <= 0) return window.alert("Error, you tried insert more or less caracters than is possible!");
   
-  if (/\b[01]+\b/g.test(binary) === true){ // Using Regex for verify binary number. 
+  if (/\b[01]+\b/g.test(binary)){ // Using Regex for verify binary number. 
     res.innerHTML = parseInt(binary,2); // Convert Binary number.
   }else {
     res.innerHTML = "_"
-    alert("Pease, enter a binary number") // Return erro alert. 
+    alert("Please, enter a binary number") // Return erro alert. 
   }
 }
